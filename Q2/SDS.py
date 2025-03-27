e41b4b0e5f7f42a623506c98479a75113b8d41aa
Ashwin <ashwinb1234@gmail.com>
2025-03-27 13:33:29 -0400

    Update SDS.py

--- Q2/SDS.py
+++ Q2/SDS.py
@@ -151,26 +151,27 @@ class SDS:
         # predict the noise residual with unet, NO grad!
         with torch.no_grad():
             ### YOUR CODE HERE ###
-            # noise = torch.randn_like(latents)
-            noise = torch.randn(latents.shape, dtype=latents.dtype, device=latents.device)
+            noise = torch.randn_like(latents)
+            # noise = torch.randn(latents.shape, dtype=latents.dtype, device=latents.device)
             noisy_latents = self.scheduler.add_noise(latents, noise, t)
-            noise_pred = self.unet(noisy_latents, t, encoder_hidden_states=text_embeddings)[0]
+            noise_pred = self.unet(noisy_latents, t, encoder_hidden_states=text_embeddings).sample
 
             if text_embeddings_uncond is not None and guidance_scale != 1:
                 ### YOUR CODE HERE ###
                 # noise_pred_cond = self.unet(noisy_latents, t, encoder_hidden_states=text_embeddings).sample
-                noise_pred_uncond = self.unet(noisy_latents, t, encoder_hidden_states=text_embeddings_uncond)[0]
+                noise_pred_uncond = self.unet(noisy_latents, t, encoder_hidden_states=text_embeddings_uncond).sample
                 noise_pred = noise_pred + guidance_scale * (noise_pred - noise_pred_uncond)
 
         # Compute SDS loss
         w = 1 - self.alphas[t]
         ### YOUR CODE HERE ###
-        # gradient = grad_scale * w[:, None, None, None] * (noise_pred - noise)  # Expanding w from (batch_size,) -> (batch_size, 1, 1, 1)  to match difference 
-        gradient = w * grad_scale * (noise_pred - noise)
+        gradient = grad_scale * w[:, None, None, None] * (noise_pred - noise)  # Expanding w from (batch_size,) -> (batch_size, 1, 1, 1)  to match difference 
+        # gradient = w * grad_scale * (noise_pred - noise)
+        gradient = torch.nan_to_num(gradient)
         target = (latents - gradient).detach()                       # 
         # target_img = self.decode_latents(target)
         # target_img = torch.tensor(target_img).permute(2, 0, 1).unsqueeze(0)    #(H,W,C) -> (C, H, W) -> (batch_size, C, H, W)
-        # loss =  F.mse_loss(latents.float(), target, reduction='sum') / latents.shape[0]
-        loss =  F.mse_loss(latents, target) 
+        loss =  F.mse_loss(latents.float(), target, reduction='sum') / latents.shape[0]
+        # loss =  F.mse_loss(latents, target) 
                     
         return loss
